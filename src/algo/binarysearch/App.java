77bcde8b7aaff0444197f433e795c69fd03c88df
Grant Rosario <grantster1124@aol.com>
2017-01-10 11:20:45 -0500

    Added recursive Binary Search

--- src/algo/binarysearch/App.java
+++ src/algo/binarysearch/App.java
@@ -3,7 +3,7 @@ package algo.binarysearch;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println(binarySearch(new int[] {1, 2, 3, 4, 7, 9, 12, 18}, 5 ));
+		System.out.println(recursiveBinarySearch(new int[] {1, 2, 3, 4, 7, 9, 12, 18}, 0, 7, 99 ));
 	}
 	
 	public static int binarySearch(int [] a, int x){
@@ -20,6 +20,21 @@ public class App {
 		}
 		
 		return -1;	
+	}
+	
+	public static int recursiveBinarySearch(int [] a, int p, int r, int x){
+		System.out.println("[ " + p + "..." + r + " ]");
+		if (p > r) return -1;
+		
+		else {
+			int q = (p + r) / 2;
+			
+			if (a[q] == x) return q;
+			
+			else if (a[q] > x) return recursiveBinarySearch(a, p, q-1, x);
+			
+			else return recursiveBinarySearch(a, q+1, r, x);
+		}
 	}
 
 }
--- src/algo/linearsearch/App.java
+++ src/algo/linearsearch/App.java
@@ -14,4 +14,15 @@ public class App {
 		}
 		return -1;
 	}
+	
+	public static int recursiveLinearSearch(int [] a, int i, int x){
+		
+		if (i > a.length - 1) return -1;
+		else if (a[i] == x) return i;
+		else {
+			System.out.println("index at: " + i);
+			return recursiveLinearSearch(a, i + 1, x);
+		}
+
+	}
 }
