a37eb1cfb30b39ff560bd98cb0a88de1d66c1513
Grant Rosario <grantster1124@aol.com>
2017-01-16 12:07:52 -0500

    Added merge sort implementation

--- src/algo/mergesort/MergeSort.java
+++ src/algo/mergesort/MergeSort.java
@@ -8,47 +8,55 @@ public class MergeSort {
 	
 	public static void sort(int inputArray[], int start, int end){
 		if(end <= start){
-			return;
+			return;  // done traversing array
 		}
 		
 		int mid = (start + end) / 2;
-		sort(inputArray, start, mid);
-		sort(inputArray, mid + 1, end);
-		merge(inputArray, start, mid, end);
+		sort(inputArray, start, mid); // sort left half
+		sort(inputArray, mid + 1, end); // sort right half
+		merge(inputArray, start, mid, end); // merge sorted results into the inputArray
 	}
 	
 	public static void merge(int inputArray[], int start, int mid, int end){
+		
 		int tempArray[] = new int [end - start + 1];
 		
+		// index counter for left side of array
 		int leftSlot = start;
+		// index counter for right side of array
 		int rightSlot = mid+1;
 		int k = 0;
 		
 		while(leftSlot <= mid && rightSlot <= end){
 			if(inputArray[leftSlot] < inputArray[rightSlot]){
 				tempArray[k] = inputArray[leftSlot];
-				leftSlot = leftSlot++;
+				leftSlot++;
 			} else {
 				tempArray[k] = inputArray[rightSlot];
-				rightSlot = rightSlot++;
+				rightSlot++;
 			}
-			k = k++;
+			k++;
 		}
 		
-		if(leftSlot <= mid){
+		/*
+		 * When it get's here, the above loop has completed so both left and right side can be considered sorted
+		 */
+		
+		if(leftSlot <= mid){ // consider the right side done being sorted. Left must already be sorted
 			while(leftSlot <= mid){
 				tempArray[k] = inputArray[leftSlot];
-				leftSlot = leftSlot++;
-				k = k++;
+				leftSlot++;
+				k++;
 			}
 		} else if (rightSlot <= end){
 			while(rightSlot <= end){
 				tempArray[k] = inputArray[rightSlot];
-				rightSlot = rightSlot + 1;
-				k = k++;
+				rightSlot++;
+				k++;
 			}
 		}
 		
+		// copy over temp array into correct slots inputArray
 		for(int i = 0; i < tempArray.length; i++){
 			inputArray[start + i] = tempArray[i];
 		}
